4832ad718f4ec11c692e290edce499fbadba94de
Joshuah Solito <jlsolito@gmail.com>
2018-02-26 18:29:24 -0600

    Josh made a login screen

---
+++ AlcoClock/app/src/main/java/comb7ptvkwmwscrum_stuff/httpstrello/alco_clock/MainActivity.java
@@ -0,0 +1,13 @@
+package comb7ptvkwmwscrum_stuff.httpstrello.alco_clock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
